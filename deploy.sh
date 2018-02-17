1f4f54b871c51ad60c2431ef6fd0ff4d300bd5b2
Stephen Fraser <stephen.fraser@digirati.com>
2018-02-17 23:27:20 +0000

    Change logic around tags

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 if [[ "$TRAVIS_BRANCH" = "master" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
     if [[ "$TRAVIS_TAG" = "$TRAVIS_BRANCH" ]]; then
-        echo "DEPLOYING A TAG!";
+        echo "SKIPPING BUILDING TAG ON MASTER";
     else
         echo "DEPLOYING A NEXT";
     fi
 fi
+
+if [[ "${TRAVIS_TAG}" != "" ]]; then
+    echo "DEPLOYING A TAG!";
+fi
+
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     echo "DEPLOYING A CANARY, NUMBER $TRAVIS_PULL_REQUEST";
 fi
